66d4b12a52903b25b933b5a900af2200d7bafd9f
James S White <james.white@github.com>
2016-06-26 15:20:45 -0500

    well this was a colossal waste of time, the Net::ISC::DHCPd module generates mangled-to-shit configs

--- scripts/dhcp/dhcp.pl
+++ scripts/dhcp/dhcp.pl
@@ -1,7 +1,60 @@
 #!/usr/bin/env perl
-use Net::MQTT::Simple::SSL;
-use JSON;
-use Data::Dumper;
+################################################################################
+# all of this fuckery is to auto-retrieve dependencies
+my $libdir;
+BEGIN {
+  use strict;
+  use Cwd;
+  use File::Basename;
+  my $oldpwd=getcwd;
+  chdir dirname($0);
+  my $pwd=getcwd;
+  mkdir("$pwd/lib",0755) unless(-d "$pwd/lib");
+  $libdir="$pwd/lib";
+  chdir $oldpwd;
+  unless (eval qq(require local::lib)){ die "FATAL: couldn't use local::lib\n"; }
+  unless (eval qq(require CPAN)){ die "FATAL: couldn't use CPAN\n"; }
+  my @modules = qw(
+                    JSON
+                    Data::Dumper
+                    TAP::Harness::Env
+                    MooseX::Types::Path::Class
+                    Net::ISC::DHCPd::Leases
+                    Net::ISC::DHCPd::Config
+                    Net::MQTT::Simple::SSL
+                  );
+  foreach my $module (@modules){
+    unless (eval "require $module"){
+      warn "couldn't use $module\n";
+      use local::lib "$libdir";
+      CPAN::install($module);
+      unless (eval "require $module"){
+        die "FATAL: Cannot install $module\n";
+      }
+    }
+  }
+}
+# end dependency auto-retrieval fuckery
+################################################################################
+
+################################################################################
+# begin actual work fuckery
+
+use File::Temp qw/ tempfile tempdir /;
+($fh, $filename) = tempfile();
+
+system("/usr/bin/scp opt\@10.255.0.1:/var/db/dhcpd.leases $filename");
+my $leases = Net::ISC::DHCPd::Leases->new( file => $filename);
+$leases->parse;
+
+system("/usr/bin/scp opt\@10.255.0.1:/etc/dhcpd.conf $filename");
+my $config = Net::ISC::DHCPd::Config->new(file => $filename);
+$config->parse;
+
+# print Data::Dumper->Dump([{'leases' => $leases,'config' => $config}]);
+print $config->generate."\n";
+
+exit 0;
 
 package DHCPlease;
 use Text::ASCIITable;
@@ -65,13 +118,6 @@ package DHCPDEntry;
   }
 1;
 
-#my $mqtt = Net::MQTT::Simple::SSL->new( "mqtt:8883",
-#                                        {
-#                                          SSL_ca_file   => '/etc/ssl/certs/ca.crt',
-#                                          SSL_cert_file => '/etc/ssl/certs/localhost.crt',
-#                                          SSL_key_file  => '/etc/ssl/private/localhost.ckey',
-#                                         }
-#                                      );
 sub leases {
    my $text_chunk='';
    my $leases = {};
@@ -104,6 +150,24 @@ sub leases {
    }
    print $table;
 }
+
+sub config_show {
+   open(my $dhcpdconf, "/usr/bin/ssh opt\@10.255.0.1 'cat /etc/dhcpd.conf'|") || die "cannot get leases. $!";
+   while(chomp(my $line=<$dhcpdconf>)){
+     print $line."\n";
+   }
+}
+leases
+
+# config_show
+
+#my $mqtt = Net::MQTT::Simple::SSL->new( "mqtt:8883",
+#                                        {
+#                                          SSL_ca_file   => '/etc/ssl/certs/ca.crt',
+#                                          SSL_cert_file => '/etc/ssl/certs/localhost.crt',
+#                                          SSL_key_file  => '/etc/ssl/private/localhost.ckey',
+#                                         }
+#                                      );
 #sub callbacks {
 #  $mqtt->retain("perl test" => "hello world");
 #  $mqtt->run(
@@ -115,4 +179,3 @@ sub leases {
 #            );
 #}
 #callbacks
-leases
