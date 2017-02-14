745a7926ed1d3cf948f014b6fe533d0e564bc56a
James S. White <jameswhite@github.com>
2017-02-14 13:08:11 -0600

    put a pin in this so I can update iterm

--- scripts/dhcp/lib/DHCPD/Worker.pm
+++ scripts/dhcp/lib/DHCPD/Worker.pm
@@ -7,8 +7,14 @@ package DHCPD::Worker;
     my $class = shift;
     my $self = {};
     bless $self, $class;
-    $self->{'remote_user'} = shift||'opt';
-    $self->{'remote_host'} = shift||'10.255.0.1';
+    $self->{'options'} = shift||{};
+    $self->{'dhcpd_user'} = $self->{'options'}->{'dhcpd_user'}||'opt';
+    $self->{'dhcpd_host'} = $self->{'options'}->{'dhcpd_host'}||'10.255.0.1';
+    $self->{'tftpd_user'} = $self->{'options'}->{'tftpd_user'}||'opt';
+    $self->{'tftpd_host'} = $self->{'options'}->{'tftpd_host'}||'10.255.12.51';
+    $self->{'pxe_root'} = $self->{'options'}->{'pxe_root'}||'/opt/tftpboot/'
+    $self->{'pxelinux_cfg'} = $self->{'options'}->{'pxe_root'}."/pxelinux.cfg";
+    $self->{'pxelinux_menus'} = $self->{'options'}->{'pxe_root'}."/pxelinux.menus";
     $self->{'json'} = JSON->new->allow_nonref;
     $self->{'config'} = $self->refresh;
     return $self;
@@ -23,11 +29,11 @@ package DHCPD::Worker;
       my $self = shift;
       # Scrape our leases
       my ($fh, $filename) = tempfile();
-      system("/usr/bin/scp $self->{'remote_user'}\@$self->{'remote_host'}:/var/db/dhcpd.leases $filename > /dev/null 2>&1");
+      system("/usr/bin/scp $self->{'dhcpd_user'}\@$self->{'dhcpd_host'}:/var/db/dhcpd.leases $filename > /dev/null 2>&1");
       my $leases = DHCPD::Leases->new($filename);
 
       # Get our dhcpd.config
-      system("/usr/bin/scp $self->{'remote_user'}\@$self->{'remote_host'}:/etc/dhcpd.conf $filename > /dev/null 2>&1");
+      system("/usr/bin/scp $self->{'dhcpd_user'}\@$self->{'dhcpd_host'}:/etc/dhcpd.conf $filename > /dev/null 2>&1");
       $self->{'config'} = DHCPD::Config->new($filename);
       return $self->{'config'};
 
@@ -35,10 +41,10 @@ package DHCPD::Worker;
 
   sub commit{
       my $self = shift;
-      $pid = open(DHCPDCONF, "| /usr/bin/ssh $self->{'remote_user'}\@$self->{'remote_host'} 'cat - | sudo tee /etc/dhcpd.conf'>/dev/null") or die "Couldn't fork: $!\n";
+      $pid = open(DHCPDCONF, "| /usr/bin/ssh $self->{'dhcpd_user'}\@$self->{'dhcpd_host'} 'cat - | sudo tee /etc/dhcpd.conf'>/dev/null") or die "Couldn't fork: $!\n";
       print DHCPDCONF $self->{'config'}->text;
       close(DHCPDCONF) or warn "Couldn't close: $!\n";
-      open (CHECKBOUNCE, "/usr/bin/ssh $self->{'remote_user'}\@$self->{'remote_host'} '/usr/bin/sudo /etc/rc.d/dhcpd restart'|");
+      open (CHECKBOUNCE, "/usr/bin/ssh $self->{'dhcpd_user'}\@$self->{'dhcpd_host'} '/usr/bin/sudo /etc/rc.d/dhcpd restart'|");
       my $output=<CHECKBOUNCE>;
       close(CHECKBOUNCE);
       print $output;
